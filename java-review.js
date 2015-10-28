3e9f1d9d1e81e19080706f39db3ef34e291c3f64
Carson Daniel Parker <Carson@dkparker.com>
2015-10-28 14:42:22 -0700

    I added a 6th question to my guessing game. This one was hard.

--- java-review.js
+++ java-review.js
@@ -1,5 +1,5 @@
-var no =0;
-var yes =0;
+var no = 0;
+var yes = 0;
 
 var question1 = prompt('Did I grow up in Washington?').toLowerCase();
 
@@ -44,13 +44,13 @@ if (question4 === 20) {
   alert('Wow! Your guess was spot on! Nice job!')
   console.log('The user answered correctly with an answer of 20.');
   yes +=1;
-  }else if (question4 > 20) {
+} else if (question4 > 20) {
   alert('Woops, you are a bit high on your guess!');
   console.log('The user answered incorrectly with ' + question4);
-  }else if (question4 < 20) {
+} else if (question4 < 20) {
   alert('Woops, you are guessing to low!');
   console.log('The user answered incorrectly with ' + question4);
-  }
+}
 
 var question5 = prompt('Is my favorite color red? Yes or No?').toLowerCase();
 
@@ -63,9 +63,22 @@ if (question5 === 'yes' || question5 === 'y' || question5 === 'ye' || question5 
   console.log('The user answered incorrectly with ' + question5);
 }
 
-alert('Congratulations, you got ' + yes + ' out of 5 questions correct! Nice job!');
+var question6 = parseInt(prompt('What is my favorite number?'));
+
+while (question6 !==17) {
+  alert("That's not my number, guess again.");
+  question6 = parseInt(prompt('What is your guess?'));
+}
+if (question6 ===17) {
+  alert('Congratulations, that was the right answer!');
+  console.log('The user answered correctly with ' + question6);
+  yes +=1;
+}
+
+alert('Congratulations, you got ' + yes + ' out of 6 questions correct! Nice job!');
 alert('The correct answer for question 1 was yes! I did grow up in Washington.');
 alert('The answer for question 2 was yes! I love drumming and I have been drumming for 7 years!');
 alert('Question 3 is an obvious yes! Who does not love computer games?');
 alert('The correct answer for question 4 was 20!');
 alert('The correct answer for question 5 was red!');
+alert('THe correct answer for question 6 was 7!');
